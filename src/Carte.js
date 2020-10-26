f102c8366d562411c6b40648c629364c269b86c9
Super Madouasse <madouasse.thibault@gmail.com>
2020-10-26 14:01:13 +0100

    C'est notre Projet !

---
+++ src/Carte.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { ImageBackground, View } from "react-native";
+import FranceMap from "./jquery.vmap";
+
+//import "./Carte.css";
+
+const Carte = () => (
+  <div className="container">
+    oui oui
+<FranceMap/>
+oui
+  </div>
+);
+export default Carte;
+
+//<span>Salut les filles</span>
